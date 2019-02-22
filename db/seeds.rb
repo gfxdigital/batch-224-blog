f5cbcabcac0deda1d3300727c855471df5eb888d
rikas <oterosantos@gmail.com>
2019-02-22 10:06:21 +0000

    Upload photo from user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Article.destroy_all
+
+url = "https://res.cloudinary.com/wagon/image/upload/v1539599085/tht31eszncv6vmlevcn2.png"
+article = Article.new(title: 'This is Shannon', body: "She has a wagon!")
+article.remote_photo_url = url
+# article.photo = url
+article.save
